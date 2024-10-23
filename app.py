c4bafe022c0c192329f3c2e9b69ad61f2ded25f7
Vincent Lu <hlu@terpmail.umd.edu>
2024-10-23 19:45:19 -0400

    accepting simple json POST requests and responding with sample message

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/predict', methods=['POST'])
+def predict():
+    data = request.json
+    image_url = data.get('image_url')
+
+    # <run model here>
+    # currently working with request json payload:
+    # {
+    #     "data": "[www.google.com]"
+    # }
+
+    prediction = {"prediction": "Your Cat", "confidence": "95%"}
+
+    return jsonify(prediction)
+
+
 
 @app.route('/')
 def home():
